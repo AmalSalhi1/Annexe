a3479801aba19776311c1e8c5453a33fc453d69c
amalensa <amalgenie2022@gmail.com>
2024-04-22 13:42:26 +0100

    ajout page administration

---
+++ Desktop/administration/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Administration from './Components/Administration';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Administration />
+    </div>
+  );
+}
+
+export default App;
+
